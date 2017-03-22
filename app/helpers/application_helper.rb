cb230be46da5bce4bcd334eabdd36b78e0f82c5a
DerekLin <djl132@case.edu>
2017-03-22 15:45:57 -0700

    Done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,42 @@ module ApplicationHelper
   content_tag(:div, capture(&block), class: css_class)
   end
 
+
+  def markdown(text)
+
+
+
+# how it's parsed
+      extensions = {
+        autolink:           true,
+        superscript:        true,
+        space_after_headers: true,
+        fenced_code_blocks: true,
+        disable_indented_code_blocks: true,
+        quote: true
+      }
+
+      # how it's rendered
+        options = {
+          filter_html:     false,
+          hard_wrap:       true,
+          link_attributes: { rel: 'nofollow', target: "_blank" }
+        }
+
+      renderer = Redcarpet::Render::HTML.new(options)
+      markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+      markdown.render(text).html_safe
+    end
+
+
+    # def markdown(text)
+    #   # specify how to parse text
+    #   options = {
+    #     filter_html
+    #   }
+
+
+
+
 end
